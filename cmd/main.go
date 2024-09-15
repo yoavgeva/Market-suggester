b7e7b98a1ae796caa8e1c513b7092e935dd63107
Yoav Geva <dryoavgeva@gmail.com>
2024-09-15 10:03:45 +0300

    start of development

--- cmd/main.go
+++ cmd/main.go
@@ -1,32 +1,40 @@
 package main
 
 import (
-
+	"database/sql"
 	"fmt"
-
 
 	"market-suggester/internal/config"
 	"market-suggester/internal/handlers"
+
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
-
 )
 var logger *zap.Logger
 
 func main() {
 	cfg := config.MustLoadConfig()
+
 	ginServer := setupRouter(cfg)
 	if err := config.RunMigrations(cfg); err != nil {
+		logger.Fatal("Failed to do migrations:", zap.Error(err))
 		panic(err)
 	}
+	dbConn, err := sql.Open("postgres", fmt.Sprintf("postgresql://%s:%s@localhost:5432/%s?sslmode=disable", cfg.DatabaseUser, cfg.DatabasePassword, cfg.DatabaseName))
+	if err != nil {
+		logger.Fatal("Failed to connect to database:", zap.Error(err))
+		panic(err)
+	}
+	defer dbConn.Close()
+	
 	setupGinMiddleware(ginServer,cfg)
 	ginServer.GET("/health", handlers.HealthHandler)
 
-	config.SetupDevelopmentData(cfg)
+	config.SetupDevelopmentData(cfg,dbConn)
 
-	err := ginServer.Run(":8080")
+	err = ginServer.Run(":8080")
 	if err != nil {
-		logger.Error("Failed to start server",
+		logger.Fatal("Failed to start server",
 			zap.Error(err),
 			zap.String("port", cfg.Port),
 		)
--- internal/config/development_data.go
+++ internal/config/development_data.go
@@ -1,13 +1,21 @@
 package config
 
-import "market-suggester/internal/db"
+import (
+	"context"
+	"database/sql"
+	"market-suggester/internal/db"
+)
 
-func SetupDevelopmentData(cfg *Config) {
+func SetupDevelopmentData(cfg *Config,dbConn *sql.DB) {
 	if cfg.Environment == "production" {
 		return
 	}
-	
-
-
+	ctx := context.Background()
+	defer ctx.Done()
+	queries := db.New(dbConn)
+	queries.CreateUser(ctx, db.CreateUserParams{
+		Email: "admin@test.com",
+		Name:  "admin",
+	})
 
 }
